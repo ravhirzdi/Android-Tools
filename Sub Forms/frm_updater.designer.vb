7398420bfddd81e5c419172330cfa914d8527f95
Muhammad Ravhi Rizaldi <vhie1046@gmail.com>
2017-03-07 01:31:11 +0700

    Add files via upload
    Add Application Updater feature

---
+++ Sub Forms/frm_updater.designer.vb
@@ -0,0 +1,149 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_updater
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.bw_update = New System.ComponentModel.BackgroundWorker()
+        Me.tmr_update = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lbl_LatestVersion = New System.Windows.Forms.Label()
+        Me.lbl_CurrentVer = New System.Windows.Forms.Label()
+        Me.lbl_LatVer = New System.Windows.Forms.Label()
+        Me.Lbl_CurVer = New System.Windows.Forms.Label()
+        Me.lbl_speed = New System.Windows.Forms.Label()
+        Me.pb_UpdateProg = New System.Windows.Forms.ProgressBar()
+        Me.btn_Update = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'bw_update
+        '
+        '
+        'tmr_update
+        '
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lbl_LatestVersion)
+        Me.GroupBox1.Controls.Add(Me.lbl_CurrentVer)
+        Me.GroupBox1.Controls.Add(Me.lbl_LatVer)
+        Me.GroupBox1.Controls.Add(Me.Lbl_CurVer)
+        Me.GroupBox1.Controls.Add(Me.lbl_speed)
+        Me.GroupBox1.Controls.Add(Me.pb_UpdateProg)
+        Me.GroupBox1.Controls.Add(Me.btn_Update)
+        Me.GroupBox1.Location = New System.Drawing.Point(3, -2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(223, 124)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        '
+        'lbl_LatestVersion
+        '
+        Me.lbl_LatestVersion.AutoSize = True
+        Me.lbl_LatestVersion.Location = New System.Drawing.Point(97, 38)
+        Me.lbl_LatestVersion.Name = "lbl_LatestVersion"
+        Me.lbl_LatestVersion.Size = New System.Drawing.Size(10, 13)
+        Me.lbl_LatestVersion.TabIndex = 13
+        Me.lbl_LatestVersion.Text = "-"
+        '
+        'lbl_CurrentVer
+        '
+        Me.lbl_CurrentVer.AutoSize = True
+        Me.lbl_CurrentVer.Location = New System.Drawing.Point(97, 16)
+        Me.lbl_CurrentVer.Name = "lbl_CurrentVer"
+        Me.lbl_CurrentVer.Size = New System.Drawing.Size(73, 13)
+        Me.lbl_CurrentVer.TabIndex = 12
+        Me.lbl_CurrentVer.Text = "lbl_CurrentVer"
+        '
+        'lbl_LatVer
+        '
+        Me.lbl_LatVer.AutoSize = True
+        Me.lbl_LatVer.Location = New System.Drawing.Point(6, 38)
+        Me.lbl_LatVer.Name = "lbl_LatVer"
+        Me.lbl_LatVer.Size = New System.Drawing.Size(80, 13)
+        Me.lbl_LatVer.TabIndex = 11
+        Me.lbl_LatVer.Text = "Latest Version :"
+        '
+        'Lbl_CurVer
+        '
+        Me.Lbl_CurVer.AutoSize = True
+        Me.Lbl_CurVer.Location = New System.Drawing.Point(6, 16)
+        Me.Lbl_CurVer.Name = "Lbl_CurVer"
+        Me.Lbl_CurVer.Size = New System.Drawing.Size(85, 13)
+        Me.Lbl_CurVer.TabIndex = 10
+        Me.Lbl_CurVer.Text = "Current Version :"
+        '
+        'lbl_speed
+        '
+        Me.lbl_speed.AutoSize = True
+        Me.lbl_speed.Location = New System.Drawing.Point(6, 80)
+        Me.lbl_speed.Name = "lbl_speed"
+        Me.lbl_speed.Size = New System.Drawing.Size(52, 13)
+        Me.lbl_speed.TabIndex = 9
+        Me.lbl_speed.Text = "lbl_speed"
+        '
+        'pb_UpdateProg
+        '
+        Me.pb_UpdateProg.Location = New System.Drawing.Point(9, 54)
+        Me.pb_UpdateProg.Name = "pb_UpdateProg"
+        Me.pb_UpdateProg.Size = New System.Drawing.Size(208, 23)
+        Me.pb_UpdateProg.TabIndex = 8
+        '
+        'btn_Update
+        '
+        Me.btn_Update.Location = New System.Drawing.Point(142, 83)
+        Me.btn_Update.Name = "btn_Update"
+        Me.btn_Update.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Update.TabIndex = 7
+        Me.btn_Update.Text = "Update"
+        Me.btn_Update.UseVisualStyleBackColor = True
+        '
+        'frm_updater
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(228, 125)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.Name = "frm_updater"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Updater"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents bw_update As System.ComponentModel.BackgroundWorker
+    Friend WithEvents tmr_update As System.Windows.Forms.Timer
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lbl_LatestVersion As System.Windows.Forms.Label
+    Friend WithEvents lbl_CurrentVer As System.Windows.Forms.Label
+    Friend WithEvents lbl_LatVer As System.Windows.Forms.Label
+    Friend WithEvents Lbl_CurVer As System.Windows.Forms.Label
+    Friend WithEvents lbl_speed As System.Windows.Forms.Label
+    Friend WithEvents pb_UpdateProg As System.Windows.Forms.ProgressBar
+    Friend WithEvents btn_Update As System.Windows.Forms.Button
+
+End Class
---
+++ Sub Forms/frm_updater.vb
@@ -0,0 +1,102 @@
+﻿'======================================='
+'Author         : Muhammad Ravhi Rizaldi'
+'Date Created   : ‎March 07, 2017        '
+'Last Modified  : March 07, 2017        '
+'Copyright      : 2017                  '
+'======================================='
+
+Imports System.IO
+Imports System.Net
+
+Public Class frm_updater
+
+    Dim locat As String = System.Reflection.Assembly.GetEntryAssembly.Location
+    Dim MyDirectory As String = System.IO.Path.GetDirectoryName(locat)
+    Dim totalsize As String
+    Dim link As String
+    Dim Csize As String
+    Public amount As String
+
+    Private Sub btn_Update_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        My.Computer.FileSystem.RenameFile(MyDirectory + "\" + Me.ProductName + ".exe", "up.tmp")
+        tmr_update.Start()
+        bw_update.RunWorkerAsync()
+        btn_Update.Enabled = False
+    End Sub
+
+    Private Sub frm_updater_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Me.Dispose()
+        frm_Main.Enabled = True
+        frm_Main.Focus()
+    End Sub
+
+    Private Sub frm_updater_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        lbl_speed.Visible = False
+        frm_Main.Enabled = False
+
+        lbl_CurrentVer.Text = Me.ProductVersion
+
+        Try
+            Dim instance As WebClient = New WebClient
+            Dim address As String = "https://www.dropbox.com/s/8zjs7m4cyvyh8s8/version.txt"
+            Dim returnValue As String
+
+            returnValue = instance.DownloadString(address)
+
+            lbl_LatestVersion.Text = returnValue
+
+            If lbl_CurrentVer.Text >= lbl_LatestVersion.Text Or lbl_LatestVersion.Text = "-" Then
+                btn_Update.Enabled = False
+                lbl_LatestVersion.Text = "Up to Date"
+            Else
+                btn_Update.Enabled = True
+            End If
+        Catch ex As Exception
+
+        End Try
+
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+    Private Sub bw_update_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles bw_update.DoWork
+        Try
+            link = "https://www.dropbox.com/s/9em3k2apcb89isv/Android%20Tools.exe"
+            Dim size1 As Integer
+            Dim wr As WebRequest
+
+            wr = WebRequest.Create(link)
+            Dim webr As WebResponse = wr.GetResponse
+            size1 = webr.ContentLength
+            webr.Close()
+            size1 = size1 / 1024
+            pb_UpdateProg.Maximum = size1
+            totalsize = size1
+            My.Computer.Network.DownloadFile("https://www.dropbox.com/s/9em3k2apcb89isv/Android%20Tools.exe", MyDirectory + "\" + Me.ProductName + ".exe")
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub bw_update_RunWorkerCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles bw_update.RunWorkerCompleted
+        Try
+            Shell("cmd.exe /c taskkill /f /im adb.exe /t", AppWinStyle.Hide, False, )
+            MsgBox("Application Updated! Please Re-Run Application")
+            Me.Dispose()
+            frm_Main.Dispose()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub tmr_update_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmr_update.Tick
+        If File.Exists(MyDirectory + "\" + Me.ProductName + ".exe") Then
+            Dim o As New FileInfo(MyDirectory + "\" + Me.ProductName + ".exe")
+            amount = o.Length
+            amount = amount / 1024
+            Csize = amount
+            pb_UpdateProg.Value = amount
+        End If
+        lbl_speed.Visible = True
+        lbl_speed.Text = Csize + "KBs/" + totalsize + "KBs"
+    End Sub
+End Class
